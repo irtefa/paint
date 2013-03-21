491219fa18c3d0c330cbb3ac476d87a70096bc21
Mohd Irtefa <irtefa1@illinois.edu>
2013-03-21 18:40:13 -0500

    broken pencil, not extending, drawing it as lines

--- js/draw.js
+++ js/draw.js
@@ -9,13 +9,10 @@ canvas.addEventListener("mousedown", getPosition, false);
 function getPosition(event){
     var x = event.x;
     var y = event.y;
-
     var canvas = document.getElementById("canvas");
 
     x -= canvas.offsetLeft;
     y -= canvas.offsetTop;
-
-    console.log("x:" + x + "y:" + y);
 
     if(!startedDrawing){
         context.beginPath();
